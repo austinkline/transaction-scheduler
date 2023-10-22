cfe09089fcea64287235fac092c1f53c98b2c026
Austin Kline <austindavidkline@gmail.com>
2023-10-22 15:25:24 -0700

    add gh workflows for testing

---
+++ normalize_coverage_report.sh
@@ -0,0 +1,2 @@
+sed -i 's/A.01cf0e2f2f715450.DeferredExecutor/contracts\/DeferredExecutor.cdc/' coverage.lcov
+sed -i 's/A.01cf0e2f2f715450.ExecutableExamples/contracts\/ExecutableExamples.cdc/' coverage.lcov
